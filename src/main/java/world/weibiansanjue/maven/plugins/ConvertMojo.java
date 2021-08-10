f609e9df5f2548eb1d6fd2656cf5654b469411e6
韦编三绝 <taojue2009@163.com>
2021-08-11 01:14:21 +0800

    feat: 自定义 html css，html 内容

--- src/main/java/world/weibiansanjue/maven/plugins/ConvertMojo.java
+++ src/main/java/world/weibiansanjue/maven/plugins/ConvertMojo.java
@@ -18,51 +18,65 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mojo(name = "convert")
 public class ConvertMojo extends AbstractMojo {
 
     @Parameter(property = "inputFilePath", defaultValue = "${basedir}/CHANGELOG.md")
-    private String inputFilePath;
+    private File inputFile;
 
     @Parameter(property = "outputFilePath", defaultValue = "${basedir}/changelog.html")
-    private String outputFilePath;
+    private File outputFile;
+
+    @Parameter(property = "title", defaultValue = "${project.artifactId}")
+    private String title;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
-        getLog().info("start convert: " + inputFilePath);
+        Map<String, String> params = new HashMap<>();
+        params.put("title", title);
+        getLog().info("start convert: " + inputFile);
         try {
-            convert(inputFilePath, outputFilePath);
-            getLog().info("convert success: " + outputFilePath);
+            convert(inputFile, outputFile, params);
+            getLog().info("convert success: " + outputFile);
         } catch (IOException e) {
             getLog().error("convert fail!", e);
         }
     }
 
-    private static void convert(String input, String output) throws IOException {
-
-        List<String> lines = Files.readAllLines(Paths.get(input), StandardCharsets.UTF_8);
+    private void convert(File input, File output, Map<String, String> params) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(input.toURI()), StandardCharsets.UTF_8);
         String markdown = String.join("\n", lines);
+        markdown = markdown.replace("<!-- <div class=\"cl-context-version\"> -->", "<div class=\"cl-context-version\">")
+                .replace("<!-- </div> -->", "</div>");
 
         MutableDataSet options = new MutableDataSet();
         options.set(Parser.EXTENSIONS, Arrays.asList(TocExtension.create()))
-                .set(TocExtension.LEVELS, 255)
                 .set(TocExtension.TITLE, "版本目录")
-                .set(TocExtension.LIST_CLASS, "toc-ul")
-                .set(TocExtension.DIV_CLASS, "toc");
+                .set(TocExtension.TITLE_LEVEL, 3)
+                .set(TocExtension.LIST_CLASS, "cl-toc-ul")
+                .set(TocExtension.DIV_CLASS, "cl-toc");
 
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
         String html = renderer.render(parser.parse(markdown));
 
+
         String htmlTemplate = ResourceUtil.getString("template/changelog.html");
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            htmlTemplate = htmlTemplate.replace("${" + entry.getKey() + "}", entry.getValue());
+            getLog().info("params " + entry.getKey() + "=" + entry.getValue());
+        }
+
         html = htmlTemplate.replace("${cl-context}", html);
 
         FileUtils.writeStringToFile(
-                new File(output),
+                output,
                 html,
                 StandardCharsets.UTF_8,
                 false);
